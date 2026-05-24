bb147cbf65c3388ff8cec25e9d1b564c84be99c9
Roberto Lopez <robertloro12@gmail.com>
2026-05-24 11:01:10 -0600

    Add modified defines of the PWM

--- AUDIO_PWM/Core/Src/stm32f4xx_it.c
+++ AUDIO_PWM/Core/Src/stm32f4xx_it.c
@@ -31,11 +31,13 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define PWM_FREQUENCY 192
 #define ADC_MAX     3.3f
 #define ADC_SIZE     4096.0f
 #define OFFSET_VOLT 0.6f
 #define RANGE_VOLT 2.3f
 
+#define PWM (uint16_t) 96000/PWM_FREQUENCY -1
 #define OFFSET ((uint16_t)((OFFSET_VOLT / ADC_MAX) * ADC_SIZE))
 #define RANGE 	((uint16_t)((RANGE_VOLT / ADC_MAX) * ADC_SIZE))
 /* USER CODE END PD */
